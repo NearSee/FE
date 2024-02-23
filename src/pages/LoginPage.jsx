f9e6a246d73f9016e5102ae097d58b5511670fd7
sem-git <lsmsm0327@naver.com>
2024-02-23 23:00:08 +0900

    feat: 로그인 초기 페이지 UI 구현

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,85 @@
-const LoginPage = () => {
-  return <>LoginPage</>;
-};
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const Background = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: #ff8d5d;
+
+  overflow: hidden;
+
+  .logo {
+    margin-top: ${({ logoMarginTop }) => logoMarginTop};
+    margin-bottom: 10rem;
+    width: 9.60744rem;
+    height: 6.5625rem;
+    transition: margin-top 0.5s ease;
+  }
+`;
+
+const ButtonRow = styled.div`
+  position: absolute;
+  bottom: 0;
+  margin-bottom: 6rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.7rem;
+  opacity: ${({ visible }) => (visible ? "1" : "0")};
+  transition: opacity 0.5s ease;
+
+  img {
+    width: 19.375rem;
+    height: 3.25706rem;
+
+    cursor: pointer;
+  }
+
+  .description {
+    color: #fff;
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 0.83894rem;
+    font-style: normal;
+    font-weight: 300;
+
+    cursor: default;
+  }
+`;
+
+function LoginPage() {
+  // const KAKAO_REST_API_KEY =  // 후에 환경변수 설정 예정
+  // const REDIRECT_URI = // 후에 환경변수 설정 예정
+  // const KAKAO_AUTH_URL =
+
+  // const handleKakaoLogin = () => {
+  //   window.location.href = KAKAO_AUTH_URL;
+  // };
+
+  const [showButtonRow, setShowButtonRow] = useState(false);
+  const [logoMarginTop, setLogoMarginTop] = useState("15rem");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowButtonRow(true);
+      setLogoMarginTop("11rem");
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <Background logoMarginTop={logoMarginTop}>
+      <img className="logo" src={`${process.env.PUBLIC_URL}/images/logo.png`} />
+
+      <ButtonRow visible={showButtonRow} /*onClick={handleKakaoLogin}*/>
+        <span className="description">니어씨에 가입하시려면</span>
+        <img src={`${process.env.PUBLIC_URL}/images/kakao_btn.png`} />
+      </ButtonRow>
+    </Background>
+  );
+}
 
 export default LoginPage;
