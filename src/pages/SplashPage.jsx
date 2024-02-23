15efb44465739680c4a293d09599c822f4a259fe
Minwooh <hsoapdc@gmail.com>
2024-02-24 02:28:16 +0900

    feat: splash css 이미지 가운데 정렬 빼고

--- src/pages/SplashPage.jsx
+++ src/pages/SplashPage.jsx
@@ -10,52 +10,118 @@ const Background = styled.div`
 `;
 
 const RoundBox = styled.div`
-  margin-left: -6rem;
-  margin-top: -10rem;
-  width: 578px;
-  height: 578px;
-  flex-shrink: 0;
-  z-index: 0;
+  position: relative;
+  left: -7rem;
 
-  border-radius: 578px;
+  width: 38rem;
+  height: 26rem;
+  flex-shrink: 0;
+
+  border-radius: 0px 0px 35rem 35rem;
   background: #ff8d5d;
 `;
 
 const Logo = styled.div`
-  height: 10rem;
-  background: #ff0;
+  height: 1.5rem;
+  width: 100%;
 `;
 const Qualify = styled.div`
-  height: 3rem;
-  background: #446;
+  display: flex;
+  height: 4.6rem;
+  width: 100%;
+
+  color: #fff;
+  font-family: Pretendard;
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: -2.4px;
+
+  justify-content: center;
+  align-items: center;
+  margin-top: 3.4rem;
+`;
+const SmaillCircle = styled.div`
+  height: 0.5rem;
+  width: 0.5rem;
+
+  margin-left: 1rem;
+  margin-right: 1rem;
+  border-radius: 0.5rem;
+  background-color: #ffd540;
 `;
 const About = styled.div`
-  height: 5rem;
-  background: #112;
+  height: rem;
+  width: 100%;
+
+  color: #fff;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 30px; /* 150% */
+  letter-spacing: -1.2px;
+
+  margin-top: 1.2rem;
 `;
 const ButtonContainer = styled.div`
-  height: 5rem;
-  background: #099;
+  display: flex;
+  height: 1.8rem;
+  width: 100%;
+
+  margin-top: 3.4rem;
+  justify-content: center;
 `;
 const Button = styled.div`
-  height: 5rem;
-  background: #004;
+  width: 0.7rem;
+  height: 0.7rem;
+  margin-left: 0.5rem;
+
+  border: none;
+  border-radius: 0.7rem;
+  background: #fff;
 `;
 
 const SplashPage = () => {
   return (
     <Background>
       <RoundBox>
-        <div style={{ marginTop: "10rem" }}>
-          <Logo></Logo>
-          <Qualify></Qualify>
-          <About></About>
+        <div
+          style={{
+            position: "absolute",
+            top: "0.5rem",
+            left: "7.8rem",
+            height: "25rem",
+            width: "60%",
+            textAlign: "center",
+          }}
+        >
+          <Logo>
+            <img src="../images/small_logo.png" alt="니어씨" width="9.6%" />
+          </Logo>
+          <Qualify>
+            <SmaillCircle />
+            질문자
+            <SmaillCircle />
+          </Qualify>
+          <About>
+            질문자는 일상 속에서 접한
+            <br /> 어려움을 질문할 수 있습니다.
+            <br />
+            <br />
+            질문을 말하는 순간
+            <br /> 모든 니어씨 답변자에게 전송돼요!
+          </About>
           <ButtonContainer>
             <Button />
             <Button />
           </ButtonContainer>
         </div>
       </RoundBox>
+
+      <img src="./images/smil_face.png" width="160rem" />
     </Background>
   );
 };
