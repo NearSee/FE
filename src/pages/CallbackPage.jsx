49272cf03f3c4172f66cb1a1001ef57baf5d5d43
sem-git <lsmsm0327@naver.com>
2024-02-29 14:11:45 +0900

    feat: CallbackPage 초기 구현

--- src/pages/CallbackPage.jsx
+++ src/pages/CallbackPage.jsx
@@ -1,5 +1,50 @@
-const CallbackPage = () => {
-  return <>CallbackPage</>;
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import FadeLoader from "react-spinners/FadeLoader";
+
+const Layout = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CallBackPage = () => {
+  const authorizeCode = new URL(window.location.href).searchParams.get("code");
+  const backendUrl = "";
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const kakaoLogin = async () => {
+      await axios({
+        method: "GET",
+        url: `${backendUrl}?code=${authorizeCode}`,
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+      }).then((res) => {
+        localStorage.setItem("accessToken", res.data.tokenInfo.accessToken);
+        localStorage.setItem("refreshToken", res.data.tokenInfo.refreshToken);
+
+        if (res.data.firstLogin) {
+          navigate("/mypage/edit");
+        } else {
+          navigate("/home");
+        }
+      });
+    };
+    kakaoLogin();
+  }, []);
+
+  return (
+    <Layout>
+      <FadeLoader color="#333333" width={6} height={16} radius={1} />
+    </Layout>
+  );
 };
 
-export default CallbackPage;
+export default CallBackPage;
