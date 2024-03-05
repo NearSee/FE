7aa853a0b1a10fdb58075516ad788d3d0949f384
sem-git <lsmsm0327@naver.com>
2024-03-05 23:46:48 +0900

    feat/refactor: 스플래쉬 모바일 스크롤 최적화 및 질문자 페이지 연결

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import Layout from "./components/Layout";
 import LoginPage from "./pages/login/LoginPage";
 import CallbackPage from "./pages/login/CallbackPage";
 
-import SplashPage01 from "./pages/splash/SplashPage01";
-import SplashPage02 from "./pages/splash/SplashPage02";
+import SplashPage from "./pages/splash/SplashPage";
 import SplashChoosePage from "./pages/splash/SplashChoosePage";
 
 import Q_MainPage from "./pages/senior/Q_MainPage";
@@ -24,8 +23,7 @@ function App() {
           <Route index element={<LoginPage />} />
           <Route path="callback" element={<CallbackPage />} />
 
-          <Route path="splash01" element={<SplashPage01 />} />
-          <Route path="splash02" element={<SplashPage02 />} />
+          <Route path="splash" element={<SplashPage />} />
           <Route path="splash_choose" element={<SplashChoosePage />} />
 
           <Route path="q_main" element={<Q_MainPage />} />
--- src/components/splash/Modal.jsx
+++ src/components/splash/Modal.jsx
@@ -1,87 +1,147 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { useNavigate } from "react-router-dom";
 
-const Background = styled.div`
-  position: relative;
-  text-align: center;
-  width: 100%;
-  height: 100%;
+import withModalBackground from "../../components/withModalBackground";
 
-  background: rgba(0, 0, 0, 0.5);
-  overflow: hidden;
-`;
-const WhiteBox = styled.div`
-  margin: 0px auto;
-  margin-top: 11.4rem;
-  padding-top: 3rem;
-  width: 20rem;
-  height: 15.5rem;
-  border-radius: 20px;
+const ModalOverlay = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 1rem;
+
+  width: 19.4375rem;
+  height: 18.1875rem;
+  flex-shrink: 0;
+
+  border-radius: 1.25rem;
   background: #fff;
+
+  z-index: 1000;
 `;
-const Text = styled.div`
+
+const Icon = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: Center;
+
+  padding-top: 2rem;
+`;
+
+const Confirm = styled.div`
+  width: 100%;
+
+  text-align: center;
   color: #000;
   font-family: Pretendard;
-  font-size: 25px;
+  font-size: 1.5625rem;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
 
-  span {
+  .orange {
     color: #ff6d2e;
   }
 `;
 
-const BaseBtn = styled.button`
-  width: 10rem;
-  height: 4rem;
+const ButtonContainer = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const Button = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 9.63rem;
+  height: 3.64875rem;
   flex-shrink: 0;
-  margin-top: 3.9rem;
-  border: none;
-  border-top: 1px solid rgba(128, 128, 128, 0.23);
-  border-right: 1px solid rgba(128, 128, 128, 0.23);
+
+  border-radius: 0rem 0rem 0rem 1.25rem;
+  border-top: 1.5px solid rgba(128, 128, 128, 0.23);
+  border-right: 2px solid rgba(128, 128, 128, 0.23);
   background: #fff;
+
   color: #363636;
   text-align: center;
   font-family: Pretendard;
-  font-size: 22.667px;
+  font-size: 1.41669rem;
   font-style: normal;
   font-weight: 500;
-  line-height: 34px; /* 150% */
-  letter-spacing: -1.36px;
+  line-height: 2.125rem; /* 150% */
+  letter-spacing: -0.085rem;
+
+  cursor: pointer;
+  z-index: 1000;
 `;
 
-const YesBtn = styled(BaseBtn)`
-  border-radius: 0px 0px 0px 20px;
-`;
-
-const NoBtn = styled(BaseBtn)`
-  border-radius: 0px 0px 20px 20px;
-`;
-
-const Modal = ({ isOpen, onClose, value }) => {
+const Modal = ({ onCloseModal, value }) => {
   const navigate = useNavigate();
-  //   이부분 수정하기_세민 답변자가 맞으면 답변자 페이지로 아니면 질문자 페이지로
-  const GoMain = () => {
-    navigate(value === "답변자" ? "/" : "/splash02");
-  };
-  const CloseModal = () => {
-    onClose();
+
+  const gotoMain = () => {
+    navigate(value === "질문자" ? "/q_main" : "#");
   };
 
   return (
-    <Background>
-      <WhiteBox>
-        <img src="../images/modal_img.png" alt="가운데 이미지" width="58rem" />
-        <Text>
-          <span>{value}</span>로<br />
+    <ModalOverlay>
+      <Icon>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="69"
+          height="69"
+          viewBox="0 0 69 69"
+          fill="none"
+        >
+          <circle
+            cx="34.5"
+            cy="34.5"
+            r="34.5"
+            fill="url(#paint0_linear_1060_1718)"
+          />
+          <circle cx="24.6853" cy="33.0132" r="7.43535" fill="white" />
+          <circle cx="42.5301" cy="33.013" r="7.43535" fill="white" />
+          <circle cx="25.8761" cy="34.2026" r="3.86638" fill="black" />
+          <circle cx="41.9367" cy="34.2026" r="3.86638" fill="black" />
+          <path
+            d="M28.2554 44.3148C30.139 46.0001 34.9174 48.3596 38.9623 44.3148"
+            stroke="#8E3514"
+            stroke-width="2.97414"
+            stroke-linecap="round"
+          />
+          <defs>
+            <linearGradient
+              id="paint0_linear_1060_1718"
+              x1="62.1595"
+              y1="27.6595"
+              x2="101.716"
+              y2="67.2155"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#FFD540" />
+              <stop offset="1" stop-color="#FF6D2E" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </Icon>
+
+      <Confirm>
+        <span>
+          <span className="orange">{value}</span>로<br />
           확정하시겠습니까?
-        </Text>
-        <YesBtn onClick={GoMain}>네</YesBtn>
-        <NoBtn onClick={CloseModal}>아니오</NoBtn>
-      </WhiteBox>
-    </Background>
+        </span>
+      </Confirm>
+
+      <ButtonContainer>
+        <Button onClick={gotoMain}>네</Button>
+        <Button
+          onClick={onCloseModal}
+          style={{ borderRadius: "0rem 0rem 1.25rem 0rem" }}
+        >
+          아니오
+        </Button>
+      </ButtonContainer>
+    </ModalOverlay>
   );
 };
 
-export default Modal;
+export default withModalBackground(Modal);
