b38a24d7179e9329cbf678e9d21c45dbbf5cc154
Minwooh <hsoapdc@gmail.com>
2024-08-28 18:35:40 +0900

    A_MorePage.jsx: 컴포넌트 정렬버튼 UI

--- src/components/answer/more/MoreAddComment.jsx
+++ src/components/answer/more/MoreAddComment.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Question from "./MoreQuestion";
 import Comment from "./MoreComment";
+import { useState } from "react";
 
 const SelectContainer = styled.div`
   position: absolute;
@@ -31,15 +32,49 @@ const WhiteBox = styled.div`
 
   background: #fff;
 `;
+
+const DropDownBox = styled.div`
+  position: absolute;
+  right: 1.5rem;
+  width: 4.4rem;
+  height: 6rem;
+
+  font-family: Inter;
+  font-size: 0.8rem;
+  font-weight: 700;
+  color: #404040;
+
+  ul {
+    list-style-type: none;
+    margin-top: 0rem;
+    padding-left: 0rem;
+  }
+
+  li {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 4.4rem;
+    height: 1.6rem;
+    border: 1px solid rgba(0, 0, 0, 0.05);
+    background: #fafafa;
+  }
+`;
 const Sort = styled.div`
-  width: 3.4rem;
+  width: 4.4rem;
   height: 1.6rem;
 
-  margin-left: 20.3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.4rem;
+
   border-radius: 0.4rem;
   border: 1px solid rgba(0, 0, 0, 0.05);
   background: #fafafa;
 `;
+
 const CommentContainer = styled.div`
   display: grid;
   justify-content: center;
@@ -48,7 +83,7 @@ const CommentContainer = styled.div`
   width: 100%;
   height: 17.8rem;
 
-  margin-top: 0.8rem;
+  margin-top: 2.2rem;
   background-color: #fff;
 `;
 
@@ -59,17 +94,16 @@ const InputBox = styled.input`
   margin-right: 1rem;
 
   border: none;
-  border-radius: 13.941px;
+  border-radius: 0.9rem;
   background: rgba(217, 217, 217, 0.5);
 
   :placeholder {
     color: #c8c8c8;
     font-family: Inter;
-    font-size: 14.951px;
+    font-size: 0.92rem;
     font-style: normal;
     font-weight: 300;
     line-height: normal;
-    letter-spacing: -0.897px;
   }
 
   color: #000;
@@ -85,11 +119,60 @@ const SendBtn = styled.button`
 `;
 
 const MoreAddComment = ({ expandQuestion }) => {
+  const [isDropDownView, setDropDownView] = useState(false);
+
+  const handleDropDownClick = () => {
+    setDropDownView(!isDropDownView);
+  };
+
+  const handleBlurContainer = () => {
+    setTimeout(() => {
+      setDropDownView(false);
+    }, 200);
+  };
+
   return (
     <SelectContainer>
       <Question question={expandQuestion} />
       <WhiteBox>
-        <Sort />
+        <DropDownBox>
+          <Sort onClick={handleDropDownClick} onBlur={handleBlurContainer}>
+            {isDropDownView ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="9"
+                height="8"
+                viewBox="0 0 9 8"
+                fill="none"
+              >
+                <path
+                  d="M5.36603 7.5C4.98113 8.16667 4.01887 8.16667 3.63397 7.5L0.602886 2.25C0.217986 1.58333 0.699111 0.749998 1.46891 0.749998L7.53109 0.749999C8.30089 0.749999 8.78202 1.58333 8.39711 2.25L5.36603 7.5Z"
+                  fill="#404040"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="9"
+                height="8"
+                viewBox="0 0 9 8"
+                fill="none"
+              >
+                <path
+                  d="M3.63397 0.500001C4.01887 -0.166666 4.98113 -0.166666 5.36603 0.500001L8.39711 5.75C8.78201 6.41667 8.30089 7.25 7.53109 7.25L1.46891 7.25C0.699111 7.25 0.217985 6.41667 0.602885 5.75L3.63397 0.500001Z"
+                  fill="#404040"
+                />
+              </svg>
+            )}
+            좋아요순
+          </Sort>
+          {isDropDownView && (
+            <ul>
+              <li>좋아요순</li>
+              <li>최신순</li>
+            </ul>
+          )}
+        </DropDownBox>
         <CommentContainer>
           <Comment />
           <Comment />
